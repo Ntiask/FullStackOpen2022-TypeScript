bbfc10732212eca5d706f474ffc225d188d27581
Ntiask <niko.tiaskorpi@gmail.com>
2022-08-03 16:05:42 +0300

    again

---
+++ typescript/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const heightInMeters = height/100
+    const bmi = weight/(heightInMeters*heightInMeters)
+    if (bmi <= 18.5){
+        return 'Underweight'
+    } else if (bmi >= 18.5 && bmi <=24.9) {
+        return 'Normal (healthy Weight)'
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        return 'Overweight'
+    } else {
+        return 'Obesity'
+    }
+}
+
+console.log(calculateBmi(175, 55))
+console.log(calculateBmi(180, 74))
